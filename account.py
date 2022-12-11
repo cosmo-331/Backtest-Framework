92f6c48ca3d2fd2298c1195dfca63a9304d729d2
cosmo-331 <108356569+cosmo-331@users.noreply.github.com>
2022-12-11 23:16:42 +0000

    Update account.py

--- account.py
+++ account.py
@@ -5,16 +5,26 @@ import pandas as pd
 import FunctionsLib
 import matplotlib.pyplot as plt
 class Account(object):
-    # data is a dictionary of dataframes whose indices are dates, 
-    # keys are the stock IDs.
+    
     def __init__(self):
+        # the amount of cash in this account
         self.cash = 1000000
+        # this is a dictionary whose keys are the stock_IDs of the stocks 
+        # we own and the value is the number of shares
         self.holdings = dict()
+        # the current date
         self.date = datetime.datetime(1900,1,1)
+        # this is a list of tuples consisting of order information
         self.order_history = list()
+        # data is a dictionary of dataframes whose indices are dates, and
+        # keys are the stock IDs.
         self.data = dict()
+        # 手续费
         self.commission = 0.002
+        # the column name of stock_ID in self.data
         self.stock_col = 'stock_ID'
+        # two lists consisting of the date and the corresponding capital of 
+        # this account at that day.
         self.value_history = [[],[]]
     
     def set_cash(self, x):
@@ -25,14 +35,15 @@ class Account(object):
     
     def load_data(self,df,col):
         '''
-        loads data into the account
+        loads data into the account. 
 
         Parameters
         ----------
         df : DataFrame
-            the processed data.
+            the processed data. Note: the index of this dataframe must be date
+            in datetime format
         col : str
-            the column of stock IDs in df.
+            the column name of stock IDs in df.
 
         Returns
         -------
@@ -40,8 +51,10 @@ class Account(object):
 
         '''
         self.data = dict()
+        # split the dataframe by stock_ID and store them separately
         for stock in df[col]:
             self.data[stock] = df[df[col] == stock]
+        # get the column name of stock_IDs
         self.stock_col = col
     
     def order(self, stock, amount):
@@ -62,26 +75,36 @@ class Account(object):
         None.
         
         '''
-        #get the price of the stock, can be improved to be more efficient
+        # get the price of the stock by looking into self.data 
         price = self.data[stock]['close'].loc[self.date]
-        # if we don't have enough money, then buy the largest amount we can
+        # if we don't have enough cash, then buy the largest amount we can
         if price * amount + price*abs(amount)*self.commission > self.cash:
             amount = math.floor(self.cash/price/100)*100
         
+        # if we own this stock, then we need to modify the number of shares we
+        # own; if we do not own this stock, then set the number of shares to be
+        # amount.
         if stock in self.holdings.keys():
             # if we are selling more than we have, we will sell all of them
             if amount + self.holdings[stock]<0:
                 amount = -self.holdings[stock]
+            # update the amount of the stock we hold
             self.holdings[stock] = self.holdings[stock] + amount
+            # if we no longer hold this stock, then remove it from our portfolio
             if self.holdings[stock] == 0:
                 self.holdings.pop(stock)
         else:
+            # if we are selling something we don't have, then don't do anything
+            if amount < 0:
+                return
             self.holdings[stock] = amount
-            
+        
+        # write the order information into the order history
         self.order_history.append({'date': self.date,
                                    'stock_ID': stock,
                                    'amount': amount,
                                    'price': price})
+        # alter the amount of cash in this account
         self.cash = self.cash - price*amount - price*abs(amount)*self.commission
         
     def order_to(self, stock, amount):
@@ -100,6 +123,7 @@ class Account(object):
         None.
 
         '''
+        # just order
         if stock in self.holdings.keys():
             self.order(stock, amount - self.holdings[stock])
         else:
@@ -107,7 +131,7 @@ class Account(object):
             
     def get_history(self, stock_ID, attributes, start_date, end_date):
         '''
-        get the data of one stock during some period in the past (between 
+        get the data of one stock or a list of stocks during some period in the past (between 
         start_date and end_date, inclusive)
         
         Parameters
@@ -131,12 +155,17 @@ class Account(object):
         '''
         # if the period is not in the past, then make end_date the previous day
         previous_day = FunctionsLib.timefromstr(FunctionsLib.last_trade_date(self.date))
-        if self.stock_col not in attributes:
-            attributes.append(self.stock_col)
         if end_date > previous_day:
             end_date = previous_day
+        # if the query didn't ask for stock_ID, we will add them
+        if self.stock_col not in attributes:
+            attributes.append(self.stock_col)
+        # if there is only one stock in the query, then just return the 
+        # corresponding dataset
         if type(stock_ID) == str:
             return self.data[stock_ID][attributes].loc[start_date:end_date+datetime.timedelta(days = 1)]
+        # if there is multiple stocks, then concatenate the dataframes
+        # for each stock
         if type(stock_ID) == list:
             ans = self.data[stock_ID[0]][attributes].loc[start_date:end_date+datetime.timedelta(days = 1)]
             for i in range(1,len(stock_ID)):
@@ -157,7 +186,9 @@ class Account(object):
 
         '''
         col = 'MA'+str(days)
+        # if we already have this moving average, don't do anything
         if col not in self.data[list(self.data.keys())[0]].columns:
+            # we add the moving average to every stock
             for stock in self.data.keys():
                 self.data[stock][col] = self.data[stock]['close'].rolling(days).mean()
     
@@ -171,8 +202,10 @@ class Account(object):
             the total capital of the account.
 
         '''
+        # we add the cash and the value of every stock we own
         ans = self.cash
         for stock in self.holdings.keys():
+            #get the price from self.data
             price = self.data[stock]['close'].loc[self.date]
             ans = ans + price*self.holdings[stock]
         return ans
@@ -186,6 +219,7 @@ class Account(object):
         None.
 
         '''
+        # we get the total capital of the account and record it in value_history
         ans = self.get_total_value()
         self.value_history[0].append(self.date)
         self.value_history[1].append(ans)
@@ -199,6 +233,7 @@ class Account(object):
         None.
 
         '''
+        # we plot the total capital over time by referring to value_history
         plt.plot(self.value_history[0], self.value_history[1])
         plt.xlabel('time')
         plt.ylabel('total_capital')
